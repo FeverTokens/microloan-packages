5fdc518d70d079182527d953283e504ab56fd37e
Bertrand JUGLAS <juglas@fevertokens.io>
2025-10-24 16:59:29 +0200

    chore: Rename contracts to follow the same pattern

--- contracts/signature/EIP712Package.sol
+++ contracts/signature/EIP712Package.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.26;
 import "./IEIP712.sol";
 import "./EIP712Internal.sol";
 
-/// @title EIP712Package
+/// @title EIP712 Package Contract
 /// @notice Provides EIP-712 domain information.
 /// @dev Inherits from IEIP712 and EIP712Internal, exposing public functions to be called through the Diamond Proxy through `delegatecall`.
-contract EIP712Package is IEIP712, EIP712Internal {
+contract EIP712 is IEIP712, EIP712Internal {
     /// @inheritdoc IEIP712
     function eip712Domain()
         external
