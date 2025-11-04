4d1dc7ec449e3239dc489f10485548a0e0079980
Youssef Maghzaz <youssefmaghzaz@gmail.com>
2025-11-04 20:01:49 +0100

    update contract

---
+++ contracts/PackageController.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity 0.8.26;
+
+import {PackageController} from "@fevertokens/packages/contracts/diamond/control/PackageController.sol";
---
+++ contracts/PackageViewer.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity 0.8.26;
+
+import {PackageViewer} from "@fevertokens/packages/contracts/diamond/view/PackageViewer.sol";
